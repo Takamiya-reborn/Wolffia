3dfc3d06a6af900f174f88b9519232ed4f0c19a5
Takamiya-reborn <2196733767@qq.com>
2026-09-21 01:15:46 +0800

    build: packaging configs(enable optimization)

--- nuitka-onedir.ps1
+++ nuitka-onedir.ps1
@@ -1,2 +1,11 @@
 # 目录模式打包
-uv run python -m nuitka --standalone --windows-console-mode=disable --include-data-dir="src/wolffia/ui=ui" --output-filename="wolffia.exe" --output-dir="nuitka/onedir" --assume-yes-for-downloads main.py
+uv run python -m nuitka `
+	--standalone `
+	--windows-console-mode=disable `
+	--lto=yes `
+	--python-flag=-OO `
+	--include-data-dir="src/wolffia/ui=ui" `
+	--output-filename="wolffia.exe" `
+	--output-dir="nuitka/onedir" `
+	--assume-yes-for-downloads `
+	main.py
--- nuitka-onefile.ps1
+++ nuitka-onefile.ps1
@@ -1,2 +1,11 @@
 # 单文件打包
-uv run python -m nuitka --onefile --windows-console-mode=disable --include-data-dir="src/wolffia/ui=ui" --output-filename="wolffia.exe" --output-dir="nuitka/onefile" --assume-yes-for-downloads main.py
+uv run python -m nuitka `
+	--onefile `
+	--windows-console-mode=disable `
+	--lto=yes `
+	--python-flag=-OO `
+	--include-data-dir="src/wolffia/ui=ui" `
+	--output-filename="wolffia.exe" `
+	--output-dir="nuitka/onefile" `
+	--assume-yes-for-downloads `
+	main.py
